ea7befda7327e8c9c2e0b7b9e62a49bd0f0de06b
Aidar <st.aidars@gmail.com>
2023-04-17 23:07:01 +0600

    fix: removes ClientApp again

--- Pharmacy/ClientApp/src/entityTypes.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCTS = "PRODUCTS";
-export const WAREHOUSES = "WAREHOUSES";
-export const PRODUCTTYPES = "PRODUCTTYPES";
-export const PRODUCTDISCOUNT = "PRODUCTDISCOUNT";
-export const PRODUCTINFO = "PRODUCTINFO";
